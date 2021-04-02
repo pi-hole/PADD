01d44c5f18ee6b6121dda8c047cbe2d43aaad7ca
Scott Smith <scottsm@mac.com>
2021-04-01 20:52:13 -0700

    set version to "3.6"

--- padd.sh
+++ padd.sh
@@ -17,7 +17,7 @@ LC_NUMERIC=C
 ############################################ VARIABLES #############################################
 
 # VERSION
-padd_version="v3.5.1"
+padd_version="v3.6"
 
 # DATE
 today=$(date +%Y%m%d)
