0204536d0359b2440626205d8355222e90852d10
Christian König <ckoenig@posteo.de>
2022-05-27 00:00:54 +0200

    Treat core_count as integer
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- padd.sh
+++ padd.sh
@@ -32,8 +32,8 @@ padd_version="v3.7.1"
 today=$(date +%Y%m%d)
 
 # CORES
-core_count=1
-core_count=$(cat /sys/devices/system/cpu/kernel_max 2> /dev/null)+1
+core_count=$(cat /sys/devices/system/cpu/kernel_max 2> /dev/null)
+core_count=$((core_count+1))
 
 # Get Config variables
 . /etc/pihole/setupVars.conf
