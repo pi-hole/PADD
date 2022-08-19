9488db9a2a01a2bb962d2bce060e6ff0a8f950c2
Christian König <ckoenig@posteo.de>
2022-08-19 20:12:54 +0200

    Bump version to v3.8.1
    Signed-off-by: Christian König <ckoenig@posteo.de>

--- padd.sh
+++ padd.sh
@@ -18,7 +18,7 @@ LC_NUMERIC=C
 ############################################ VARIABLES #############################################
 
 # VERSION
-padd_version="v3.8.0"
+padd_version="v3.8.1"
 
 # LastChecks
 LastCheckVersionInformation=$(date +%s)
